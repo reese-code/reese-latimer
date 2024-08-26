bed51e58452fe30acbb91bbd369d09dbfecff72e
reese-code <reese.coding@gmail.com>
2024-08-26 14:18:02 -0600

    config change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['@gsap/react'], // Add this line to mark @gsap/react as an external dependency
+    },
+  },
+});
