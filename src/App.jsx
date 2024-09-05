881d8b61753c5b5701bff98bc0b6892a139c61b6
reese-code <reese.coding@gmail.com>
2024-09-04 22:22:36 -0600

    css file addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import "./index.css"
 import Nav from './Nav';
 import Footer from './Footer';
 import Home from './Home.jsx';
