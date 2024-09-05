8b2df906e24ae0ed744eab23a4c3f8808fc2625d
reese-code <reese.coding@gmail.com>
2024-09-05 10:46:10 -0600

    added tailwind attempt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}', // or adjust to match your project structure
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
