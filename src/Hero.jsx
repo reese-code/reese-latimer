65b71256c615781b1ffffa73f8bcbdf7a1394ea4
reese-code <reese.coding@gmail.com>
2024-09-04 22:28:23 -0600

    test image directory

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -123,9 +123,9 @@ function Hero() {
     return(
         <>
             <section className="hero">
-                <img className='logo-main' src="../assets/latimer-logo.png" />
+                <img className='logo-main' src="/assets/latimer-logo.png" />
                 <div className="content">
-                    <img className='peace' src="../assets/peace-sign.png" />
+                    <img className='peace' src="/assets/peace-sign.png" />
                     <div class="p">turning your web dreams into <span className='bold-italic'>reality</span></div>
                 </div>
             </section>
