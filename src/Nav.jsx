e764375313517494fc50d6adfaecfb5087fd029e
reese-code <reese.coding@gmail.com>
2024-09-05 10:54:55 -0600

    no underline nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -32,9 +32,9 @@ function Nav() {
                     <Link to="/home" className="a">Reese Latimer</Link>
                 </div>
                 <div className="links flex flex-row no-underline">
-                    <Link to="/about" className="a p underline">about</Link>
-                    <Link to="/work" className="a p underline">work</Link>
-                    <Link to="/contact" className="a p underline">contact</Link>
+                    <Link to="/about" className="a p underline no-underline">about</Link>
+                    <Link to="/work" className="a p underline no-underline">work</Link>
+                    <Link to="/contact" className="a p underline no-underline">contact</Link>
                 </div>
             </div>
             <div className="mobile-nav">
@@ -42,9 +42,9 @@ function Nav() {
                     <img onClick={navClose} src="../assets/close-nav.png" alt="Close navigation" />
                 </div>
                 <div className="links-mobile-nav">
-                    <Link to="/about" className="a h2 underline" onClick={navClose}>about</Link>
-                    <Link to="/work" className="a h2 underline" onClick={navClose}>work</Link>
-                    <Link to="/contact" className="a h2 underline" onClick={navClose}>contact</Link>
+                    <Link to="/about" className="a h2 underline no-underline" onClick={navClose}>about</Link>
+                    <Link to="/work" className="a h2 underline no-underline" onClick={navClose}>work</Link>
+                    <Link to="/contact" className="a h2 underline no-underline" onClick={navClose}>contact</Link>
                 </div>
             </div>
         </div>
