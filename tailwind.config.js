85d800cee6823fc34522490a10fdccc077bab286
reese-code <reese.coding@gmail.com>
2024-09-05 21:05:28 -0600

    mobile scroll fix 4.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,6 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}', // or adjust to match your project structure
   ],
   theme: {
-    screens: {
-      'lg': '1025px',
-    }
     extend: {},
   },
   plugins: [],
