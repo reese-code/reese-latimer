9bb54556b99bea30f50e0bb33a623580119cbbfa
reese-code <reese.coding@gmail.com>
2024-11-02 23:25:26 -0600

    added form to contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { Sidetab } from '@typeform/embed-react'
+import React, { useEffect } from 'react';
 
 function Contact() {
-    return(
-        <>
-            <typeformEmbed id="<form-id>" />
-        </>
-    )
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "//embed.typeform.com/next/embed.js";
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <div data-tf-live="01JBR2MBJ637QWRS49PYEN4H2S"></div>
+    );
 }
 
 export default Contact
